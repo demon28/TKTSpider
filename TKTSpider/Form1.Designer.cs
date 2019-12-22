7629b5213b541775f68f94c268b5da37a96b507f
274733956 <274733956@qq.com>
2019-12-22 22:20:46 +0800

    建项目

--- TKTSpider/Form1.Designer.cs
+++ TKTSpider/Form1.Designer.cs
@@ -28,20 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.skinWaterTextBox1 = new CCWin.SkinControl.SkinWaterTextBox();
+            this.skinLabel1 = new CCWin.SkinControl.SkinLabel();
+            this.tb_url = new CCWin.SkinControl.SkinTextBox();
+            this.btn_star = new CCWin.SkinControl.SkinButton();
+            this.skinDataGridView1 = new CCWin.SkinControl.SkinDataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skinDataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.skinWaterTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 314);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(786, 129);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loger";
+            // 
+            // skinWaterTextBox1
+            // 
+            this.skinWaterTextBox1.Location = new System.Drawing.Point(6, 20);
+            this.skinWaterTextBox1.Multiline = true;
+            this.skinWaterTextBox1.Name = "skinWaterTextBox1";
+            this.skinWaterTextBox1.Size = new System.Drawing.Size(774, 90);
+            this.skinWaterTextBox1.TabIndex = 0;
+            this.skinWaterTextBox1.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.skinWaterTextBox1.WaterText = "";
+            // 
+            // skinLabel1
+            // 
+            this.skinLabel1.AutoSize = true;
+            this.skinLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel1.BorderColor = System.Drawing.Color.White;
+            this.skinLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel1.Location = new System.Drawing.Point(10, 39);
+            this.skinLabel1.Name = "skinLabel1";
+            this.skinLabel1.Size = new System.Drawing.Size(44, 17);
+            this.skinLabel1.TabIndex = 1;
+            this.skinLabel1.Text = "地址：";
+            // 
+            // tb_url
+            // 
+            this.tb_url.BackColor = System.Drawing.Color.Transparent;
+            this.tb_url.DownBack = null;
+            this.tb_url.Icon = null;
+            this.tb_url.IconIsButton = false;
+            this.tb_url.IconMouseState = CCWin.SkinClass.ControlState.Normal;
+            this.tb_url.IsPasswordChat = '\0';
+            this.tb_url.IsSystemPasswordChar = false;
+            this.tb_url.Lines = new string[] {
+        "skinTextBox1"};
+            this.tb_url.Location = new System.Drawing.Point(57, 35);
+            this.tb_url.Margin = new System.Windows.Forms.Padding(0);
+            this.tb_url.MaxLength = 32767;
+            this.tb_url.MinimumSize = new System.Drawing.Size(28, 28);
+            this.tb_url.MouseBack = null;
+            this.tb_url.MouseState = CCWin.SkinClass.ControlState.Normal;
+            this.tb_url.Multiline = false;
+            this.tb_url.Name = "tb_url";
+            this.tb_url.NormlBack = null;
+            this.tb_url.Padding = new System.Windows.Forms.Padding(5);
+            this.tb_url.ReadOnly = false;
+            this.tb_url.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tb_url.Size = new System.Drawing.Size(641, 28);
+            // 
+            // 
+            // 
+            this.tb_url.SkinTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_url.SkinTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_url.SkinTxt.Font = new System.Drawing.Font("微软雅黑", 9.75F);
+            this.tb_url.SkinTxt.Location = new System.Drawing.Point(5, 5);
+            this.tb_url.SkinTxt.Name = "BaseText";
+            this.tb_url.SkinTxt.Size = new System.Drawing.Size(613, 18);
+            this.tb_url.SkinTxt.TabIndex = 0;
+            this.tb_url.SkinTxt.Text = "skinTextBox1";
+            this.tb_url.SkinTxt.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.tb_url.SkinTxt.WaterText = "";
+            this.tb_url.TabIndex = 2;
+            this.tb_url.Text = "skinTextBox1";
+            this.tb_url.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.tb_url.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.tb_url.WaterText = "";
+            this.tb_url.WordWrap = true;
+            // 
+            // btn_star
+            // 
+            this.btn_star.BackColor = System.Drawing.Color.Transparent;
+            this.btn_star.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.btn_star.DownBack = null;
+            this.btn_star.Location = new System.Drawing.Point(712, 35);
+            this.btn_star.MouseBack = null;
+            this.btn_star.Name = "btn_star";
+            this.btn_star.NormlBack = null;
+            this.btn_star.Size = new System.Drawing.Size(75, 27);
+            this.btn_star.TabIndex = 3;
+            this.btn_star.Text = "开 始";
+            this.btn_star.UseVisualStyleBackColor = false;
+            // 
+            // skinDataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(246)))), ((int)(((byte)(253)))));
+            this.skinDataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.skinDataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.skinDataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.skinDataGridView1.ColumnFont = null;
+            this.skinDataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(239)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.skinDataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.skinDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.skinDataGridView1.ColumnSelectForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(188)))), ((int)(((byte)(240)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.skinDataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.skinDataGridView1.EnableHeadersVisualStyles = false;
+            this.skinDataGridView1.GridColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.skinDataGridView1.HeadFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinDataGridView1.HeadSelectForeColor = System.Drawing.SystemColors.HighlightText;
+            this.skinDataGridView1.Location = new System.Drawing.Point(13, 88);
+            this.skinDataGridView1.Name = "skinDataGridView1";
+            this.skinDataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.skinDataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            this.skinDataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.skinDataGridView1.RowTemplate.Height = 23;
+            this.skinDataGridView1.Size = new System.Drawing.Size(774, 220);
+            this.skinDataGridView1.TabIndex = 4;
+            this.skinDataGridView1.TitleBack = null;
+            this.skinDataGridView1.TitleBackColorBegin = System.Drawing.Color.White;
+            this.skinDataGridView1.TitleBackColorEnd = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(196)))), ((int)(((byte)(242)))));
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.skinDataGridView1);
+            this.Controls.Add(this.btn_star);
+            this.Controls.Add(this.tb_url);
+            this.Controls.Add(this.skinLabel1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Main";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.skinDataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private CCWin.SkinControl.SkinWaterTextBox skinWaterTextBox1;
+        private CCWin.SkinControl.SkinLabel skinLabel1;
+        private CCWin.SkinControl.SkinTextBox tb_url;
+        private CCWin.SkinControl.SkinButton btn_star;
+        private CCWin.SkinControl.SkinDataGridView skinDataGridView1;
     }
 }
 
